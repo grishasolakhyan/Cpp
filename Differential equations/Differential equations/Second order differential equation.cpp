e8fba1b48ca401eb7aaebddb8b75ef09d94411d2
grishasolakhyan <borsch.tv@mail.ru>
2022-11-06 01:53:27 +1000

    Hello, World!

---
+++ Differential equations/Differential equations/Second order differential equation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<windows.h>
+
+using namespace std;
+
+void differ(double x[], double y[], double xn, double y1, double yn, int p, int q, double h, int n);
+int main()
+{
+
+}
